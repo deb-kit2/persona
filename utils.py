df109bca94dc8c26be7e28befbe390074d37291b
Deb <nick.kittu.deb@gmail.com>
2023-05-13 14:31:02 +0530

    clean not used code

--- utils.py
+++ utils.py
@@ -5,33 +5,10 @@ import torch
 from torch.utils.data import Dataset
 
 
-def construct_adj(n_nodes, n_persona, max_n = 56) :
-    # returns the unweighted and relation-unaware graph
-    # persona self, make bidirectional next experiment: reason persona might change over conv
-    
-    adj = torch.zeros((max_n, max_n))
-    # to avoid div by 0 or -1 in graphs
-    for i in range(max_n - n_nodes) :
-        adj[i][i] = 2
-
-    for i in range(max_n - n_nodes, max_n) :
-        adj[i][i] = 1
-        if i + 1 < max_n :
-            adj[i + 1, i] = 1
-        if i + 2 < max_n :
-            adj[i + 2, i] = 1
-            adj[i, i + 2] = 1
-    for i in range(56 - n_nodes + 1, max_n, 2) :
-        for j in range(n_persona) :
-            adj[i, j] = 1
-
-    return adj
-
 class PersonaDataset(Dataset) :
     # Dataset class for the PersonaChat dataset
 
     def __init__(self, args, subset = "train") :
-        
         name = "data/" + args.endec + "." + args.pretrained_name.split("/")[-1]
         name += "." + args.encrep + subset + ".json"
 
