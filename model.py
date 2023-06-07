f30dde643059eee9bf847ec18d61ada5571903f3
Deb <nick.kittu.deb@gmail.com>
2023-06-08 02:29:54 +0530

    custom decoder

--- model.py
+++ model.py
@@ -1,49 +1,42 @@
 import logging
-from typing import Optional, List, Tuple, Union
+from typing import Optional
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as f
 
 from modules.graphs import GCNLayer, GCNLayerOrig, GATLayer
-from transformers import BartModel, BartPretrainedModel, BartConfig
-
-from torch.nn import CrossEntropyLoss
-from transformers.modeling_outputs import (
-    BaseModelOutput,
-    Seq2SeqLMOutput,
-    Seq2SeqModelOutput
-)
 
 
-def shift_tokens_right(input_ids: torch.Tensor, pad_token_id: int, decoder_start_token_id: int):
-    """
-    Shift input ids one token to the right.
-    """
-    shifted_input_ids = input_ids.new_zeros(input_ids.shape)
-    shifted_input_ids[:, 1:] = input_ids[:, :-1].clone()
-    shifted_input_ids[:, 0] = decoder_start_token_id
+class Head(nn.Module) :
+    def __init__(self, dim, block_size) :
+        super().__init__()
 
-    if pad_token_id is None:
-        raise ValueError("self.model.config.pad_token_id has to be defined.")
-    # replace possible -100 values in labels by `pad_token_id`
-    shifted_input_ids.masked_fill_(shifted_input_ids == -100, pad_token_id)
+        self.key = nn.Linear(dim, dim, bias = False)
+        self.query = nn.Linear(dim, dim, bias = False)
+        self.value = nn.Linear(dim, dim, bias = False)
 
-    return shifted_input_ids
+        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
+
+    def forward(self, x, mask = None) :
+        B, T, C = x.shape
+
+        k = self.key(x)
+        q = self.query(x)
+        v = self.value(x)
+
+        wei = q @ k.transpose(-2, -1) * C**-0.5 # B, T, T
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
+        wei = f.softmax(wei, dim = -1)
+
+        out = wei @ v
+        return out
 
 
-class BartForPersonaAwareGeneration(BartPretrainedModel):
-    base_model_prefix = "model"
-    _keys_to_ignore_on_load_missing = [
-        r"final_logits_bias",
-        r"lm_head.weight",
-        "encoder.embed_tokens.weight",
-        "decoder.embed_tokens.weight",
-    ]
+class PersonaAwareGeneration(nn.Module) :
+    def __init__(self, args) :
+        super().__init__()
 
-    def __init__(self, config: BartConfig, args):
-        super().__init__(config)
-        
         self.num_heads = args.num_heads
         self.max_conv_length = args.max_conv_length
         self.dropout = args.dropout
@@ -52,201 +45,86 @@ class BartForPersonaAwareGeneration(BartPretrainedModel):
             self.d_in = 768
         elif args.pretrained_name.endswith("large") :
             self.d_in = 1024
-        else :
+        else : # small
             self.d_in = 512
-
-        self.mha = nn.MultiheadAttention(batch_first = True, embed_dim = self.d_in, 
-                                         num_heads = args.num_heads, dropout = self.dropout
-                                        )
-
+        
+        self.conv_mha = nn.MultiheadAttention(batch_first = True, embed_dim = self.d_in, 
+                                              num_heads = args.num_heads, dropout = self.dropout
+                                              )
         self.gcn1 = GCNLayerOrig(self.d_in, self.d_in)
         self.gcn2 = GCNLayerOrig(self.d_in, self.d_in)
+        
+        self.h_dense = nn.Linear(self.d_in, 4 * self.d_in)
+        self.h_dense2 = nn.Linear(4 * self.d_in, self.d_in)
 
-        self.model = BartModel(config)
-        # freezing encoder
-        for p in self.model.encoder.parameters() :
-            p.requires_grad = False
+        self.r_enc_mha = nn.MultiheadAttention(batch_first = True, embed_dim = self.d_in,
+                                               num_heads = args.num_heads, dropout = self.dropout
+                                               )
 
-        self.register_buffer("final_logits_bias", torch.zeros((1, self.model.shared.num_embeddings)))
-        self.lm_head = nn.Linear(config.d_model, self.model.shared.num_embeddings, bias=False)
+        self.head = Head(self.d_in, args.max_length)
 
-        # Initialize weights and apply final processing
-        self.post_init()
+        self.o_dense = nn.Linear(self.d_in, 4 * self.d_in)
+        self.o_dense2 = nn.Linear(4 * self.d_in, self.d_in)
+        
+        self.lm_head = nn.Linear(self.d_in, args.vocab_size, bias = False)
 
-    def get_encoder(self):
-        return self.model.get_encoder()
+    def forward(self,
+                conv_cls, 
+                persona_cls,
+                adj,
+                conv_mask,
+                labels: Optional[torch.LongTensor] = None,
+                decoder_input_ids: Optional[torch.LongTensor] = None,
+                decoder_attention_mask: Optional[torch.LongTensor] = None,
+                ) :
+        
+        # add positional embedding?
+        mask_ = conv_mask.unsqueeze(dim = 1).repeat(self.num_heads, self.max_conv_length, 1)
+        attented, _ = self.conv_mha(conv_cls, conv_cls, conv_cls, 
+                                    need_weights = False, 
+                                    attn_mask = mask_)
+        
+        x = attented * (~ conv_mask.unsqueeze(dim = 2))
+        x = torch.cat((persona_cls, x), dim = -2)
 
-    def get_decoder(self):
-        return self.model.get_decoder()
+        x = f.dropout(x, self.dropout)
+        x = self.gcn1(x, adj)
+        x = f.dropout(x, self.dropout)
+        x = self.gcn2(x, adj) 
+        x = f.dropout(x, self.dropout)
 
-    def resize_token_embeddings(self, new_num_tokens: int) -> nn.Embedding:
-        new_embeddings = super().resize_token_embeddings(new_num_tokens)
-        self._resize_final_logits_bias(new_num_tokens)
-        return new_embeddings
+        # before feed forward
+        h_enc = self.h_dense(x) # batch, n_nodes, 4d
+        h_enc = self.h_dense2(f.leaky_relu(h_enc, 0.2)) + x # batch, n_nodes, d
+        # To-Do normalization
 
-    def _resize_final_logits_bias(self, new_num_tokens: int) -> None:
-        old_num_tokens = self.final_logits_bias.shape[-1]
-        if new_num_tokens <= old_num_tokens:
-            new_bias = self.final_logits_bias[:, :new_num_tokens]
-        else:
-            extra_bias = torch.zeros((1, new_num_tokens - old_num_tokens), device=self.final_logits_bias.device)
-            new_bias = torch.cat([self.final_logits_bias, extra_bias], dim=1)
-        self.register_buffer("final_logits_bias", new_bias)
+        h_enc_mask = None # To-Do
 
-    def get_output_embeddings(self):
-        return self.lm_head
+        if labels is None :
+            return {
+                "h_enc" : h_enc,
+            }
+    
+        r = self.embedding_table(decoder_input_ids)
+        # add positional embedding?
 
-    def set_output_embeddings(self, new_embeddings):
-        self.lm_head = new_embeddings
+        # To-Do use decoder_attention_mask
+        h_r = self.head(r, decoder_attention_mask)
+        
+        o = self.r_enc_mha(h_r, h_enc, h_enc) # b, t, d
+        o_ = self.o_dense(o) # b, t, 4d
+        o_ = self.o_dense2(f.leaky_relu(o_, 0.2)) + o # b, t, d
 
-    def forward(
-        self,
-        conv_cls = None, persona_cls = None, adj = None, conv_mask = None,
-        encoder_hidden_states = None,
-        attention_mask: Optional[torch.Tensor] = None,
-        decoder_input_ids: Optional[torch.LongTensor] = None,
-        decoder_attention_mask: Optional[torch.LongTensor] = None,
+        # To-Do normalization
 
-        input_ids: torch.LongTensor = None,
-        head_mask: Optional[torch.Tensor] = None,
-        decoder_head_mask: Optional[torch.Tensor] = None,
-        cross_attn_head_mask: Optional[torch.Tensor] = None,
-        encoder_outputs: Optional[List[torch.FloatTensor]] = None,
-        past_key_values: Optional[List[torch.FloatTensor]] = None,
-        inputs_embeds: Optional[torch.FloatTensor] = None,
-        decoder_inputs_embeds: Optional[torch.FloatTensor] = None,
-        labels: Optional[torch.LongTensor] = None,
-        use_cache: Optional[bool] = None,
-        output_attentions: Optional[bool] = None,
-        output_hidden_states: Optional[bool] = None,
-        return_dict: Optional[bool] = None,
-    ) -> Union[Tuple, Seq2SeqLMOutput]:
-        r"""
-        labels (`torch.LongTensor` of shape `(batch_size, sequence_length)`, *optional*):
-            Labels for computing the masked language modeling loss. Indices should either be in `[0, ...,
-            config.vocab_size]` or -100 (see `input_ids` docstring). Tokens with indices set to `-100` are ignored
-            (masked), the loss is only computed for the tokens with labels in `[0, ..., config.vocab_size]`.
-
-        Returns:
-        """
-        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-
-        if encoder_hidden_states is not None :
-            mask_ = conv_mask.unsqueeze(dim = 1).repeat(self.num_heads, self.max_conv_length, 1)
-            attented, _ = self.mha(conv_cls, conv_cls, conv_cls, 
-                                  need_weights = False, 
-                                  attn_mask = mask_)
-            # add positional embedding?
-
-            x = attented * (~ conv_mask.unsqueeze(dim = 2))
-            x = torch.cat((persona_cls, attented), dim = -2)
-
-            x = f.dropout(x, self.dropout)
-            x = self.gcn1(x, adj)
-            x = f.dropout(x, self.dropout)
-            x = self.gcn2(x, adj)
-
-            cls = x[:, -1, :]
-            encoder_hidden_states[:, 0, :] = cls
-
-            encoder_outputs = BaseModelOutput(
-                last_hidden_state = encoder_hidden_states,
-                hidden_states = None,
-                attentions = None
-            )
-
-        if labels is not None:
-            if use_cache:
-                pass
-                # logger.warning("The `use_cache` argument is changed to `False` since `labels` is provided.")
-            use_cache = False
-            if decoder_input_ids is None and decoder_inputs_embeds is None:
-                decoder_input_ids = shift_tokens_right(
-                    labels, self.config.pad_token_id, self.config.decoder_start_token_id
-                )
-
-        outputs = self.model(
-            input_ids,
-            decoder_input_ids=decoder_input_ids,
-            decoder_attention_mask=decoder_attention_mask,
-            attention_mask=attention_mask,
-            encoder_outputs=encoder_outputs,
-            head_mask=head_mask,
-            decoder_head_mask=decoder_head_mask,
-            cross_attn_head_mask=cross_attn_head_mask,
-            past_key_values=past_key_values,
-            inputs_embeds=inputs_embeds,
-            decoder_inputs_embeds=decoder_inputs_embeds,
-            use_cache=use_cache,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
-        )
-
-        lm_logits = self.lm_head(outputs[0])
-        lm_logits = lm_logits + self.final_logits_bias.to(lm_logits.device)
-
-        masked_lm_loss = None
-        if labels is not None:
-            labels = labels.to(lm_logits.device)
-            loss_fct = CrossEntropyLoss()
-            masked_lm_loss = loss_fct(lm_logits.view(-1, self.config.vocab_size), labels.view(-1))
-
-        if not return_dict:
-            output = (lm_logits,) + outputs[1:]
-            return ((masked_lm_loss,) + output) if masked_lm_loss is not None else output
-
-        return Seq2SeqLMOutput(
-            loss=masked_lm_loss,
-            logits=lm_logits,
-            past_key_values=outputs.past_key_values,
-            decoder_hidden_states=outputs.decoder_hidden_states,
-            decoder_attentions=outputs.decoder_attentions,
-            cross_attentions=outputs.cross_attentions,
-            encoder_last_hidden_state=outputs.encoder_last_hidden_state,
-            encoder_hidden_states=outputs.encoder_hidden_states,
-            encoder_attentions=outputs.encoder_attentions,
-        )
-
-    def prepare_inputs_for_generation(
-        self,
-        decoder_input_ids,
-        past_key_values=None,
-        attention_mask=None,
-        decoder_attention_mask=None,
-        head_mask=None,
-        decoder_head_mask=None,
-        cross_attn_head_mask=None,
-        use_cache=None,
-        encoder_outputs=None,
-        **kwargs,
-    ):
-        # cut decoder_input_ids if past_key_values is used
-        if past_key_values is not None:
-            decoder_input_ids = decoder_input_ids[:, -1:]
-
+        logits = self.lm_head(o_) # batch, t, vocab_size
         return {
-            "input_ids": None,  # encoder_outputs is defined. input_ids not needed
-            "encoder_outputs": encoder_outputs,
-            "past_key_values": past_key_values,
-            "decoder_input_ids": decoder_input_ids,
-            "attention_mask": attention_mask,
-            "decoder_attention_mask": decoder_attention_mask,
-            "head_mask": head_mask,
-            "decoder_head_mask": decoder_head_mask,
-            "cross_attn_head_mask": cross_attn_head_mask,
-            "use_cache": use_cache,  # change this to avoid caching (presumably for debugging)
+            "h_enc" : h_enc,
+            "logits" :  logits
         }
+    
+    def generate(self, inputs = None, max_new_tokens = 32) :
+        # inputs : batch, t
+        for _ in range(max_new_tokens) :
 
-    def prepare_decoder_input_ids_from_labels(self, labels: torch.Tensor):
-        return shift_tokens_right(labels, self.config.pad_token_id, self.config.decoder_start_token_id)
-
-    @staticmethod
-    def _reorder_cache(past_key_values, beam_idx):
-        reordered_past = ()
-        for layer_past in past_key_values:
-            # cached cross_attention states don't have to be reordered -> they are always the same
-            reordered_past += (
-                tuple(past_state.index_select(0, beam_idx) for past_state in layer_past[:2]) + layer_past[2:],
-            )
-        return reordered_past
+        return p
